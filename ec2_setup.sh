792f0ca68e2d5c34d0750316896d4b1bbcea5c4d
sonebu <buraksoner92@gmail.com>
2024-10-09 22:15:14 +0300

    update setup for ghdl and add ghdl test files

--- ec2_setup.sh
+++ ec2_setup.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install -y zip gnat make
+sudo apt-get install -y zip gnat make libz-dev
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
@@ -14,12 +14,15 @@ conda create -p ~/venv_p39/ python=3.9 -y
 source activate ~/venv_p39/
 pip install fastapi jinja2 uvicorn python-multipart aiofiles requests
 
+cd ~
 git config --global user.name "sonebu"
 git config --global user.email "buraksoner92@gmail.com"
 git clone https://github.com/ghdl/ghdl.git
 mv ghdl/ ghdl-4.0.0/
 cd ghdl-4.0.0/
 git checkout v4.0.0
-cd ~
-git clone https://github.com/gcc-mirror/gcc.git
-git checkout releases/gcc-11.4.0
+mkdir build
+cd build
+../configure
+make -j2 
+sudo make install -j2 
