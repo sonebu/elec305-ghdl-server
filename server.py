cad1eddde5069f0c7eb25de686443f9ff02c72ad
sonebu <buraksoner92@gmail.com>
2024-10-29 22:15:43 +0000

    check subs, hw1_q2

--- server.py
+++ server.py
@@ -11,11 +11,16 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory='templates')
     
 ###################################################################################################################################################################
-### Routes
+### Generic Routes
 ###
 
+### Home: select HW
 class hwChoices(str, enum.Enum):
     hw1 = "HW1"
+
+@app.get("/")
+def home_get(request: Request):
+    return templates.TemplateResponse('home.html', context={'request': request})
 
 @app.post('/')
 def home_post(request: Request, hw_selection: hwChoices = Form(hwChoices)):
@@ -23,30 +28,65 @@ def home_post(request: Request, hw_selection: hwChoices = Form(hwChoices)):
     redirect_url = base_url + "/" + hw_name
     return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
 
-@app.get("/")
-def home_get(request: Request):
-    return templates.TemplateResponse('home.html', context={'request': request})
-
+### Check submissions
 @app.get("/checksubmissions")
-def home_get(request: Request):
-    # ToDo: checksubmissions functionality (submit username-password to get table of HW vs. Qs with status notes)
-    return templates.TemplateResponse('checksubmissions.html', context={'request': request})
+def checksubmissions_get(request: Request, password: str = Form(""), username: str = Form("")):
+    return templates.TemplateResponse('checksubmissions.html', context={'request': request, 'qout': 'Submission state table will appear here'})
 
+@app.post("/checksubmissions")
+def checksubmissions_post(request: Request, password: str = Form(""), username: str = Form("")):
+    inputs_healthy, msg = inputchecks(username, password, "dummy text")
+    if(inputs_healthy):
+        hw1_q1_note = checksubmission_for_hw_q_username(hw_tag = "hw1", question_tag = "q1", username = username);
+        hw1_q2_note = checksubmission_for_hw_q_username(hw_tag = "hw1", question_tag = "q2", username = username);
+        hw1_q3_note = checksubmission_for_hw_q_username(hw_tag = "hw1", question_tag = "q3", username = username);
+        hw1_q4_note = checksubmission_for_hw_q_username(hw_tag = "hw1", question_tag = "q4", username = username);
+        hw1_q5_note = checksubmission_for_hw_q_username(hw_tag = "hw1", question_tag = "q5", username = username);
+        output = f"""
+<table>
+<tr> <th></th>          <th>HW1</th></tr>
+<tr> <td><b>Q1</b></td> <td>{hw1_q1_note}</td></tr>
+<tr> <td><b>Q2</b></td> <td>{hw1_q2_note}</td></tr>
+<tr> <td><b>Q3</b></td> <td>{hw1_q3_note}</td></tr>
+<tr> <td><b>Q4</b></td> <td>{hw1_q4_note}</td></tr>
+<tr> <td><b>Q5</b></td> <td>{hw1_q5_note}</td></tr>
+</table>
+"""
+    else:
+        output = msg
+    return templates.TemplateResponse('checksubmissions.html', context={'request': request, 'qout': output})
+
+###################################################################################################################################################################
+### HW1 Routes
+### 
 @app.get("/hw1")
 def hw1_get(request: Request):
     return templates.TemplateResponse('hw1.html', context={'request': request})
 
+### HW1 - Q1
 @app.get("/hw1_q1")
 def hw1_q1_get(request: Request, password: str = Form(""), username: str = Form(""), input_text: str = Form(""), buttonaction: str = Form("")):
     return templates.TemplateResponse('hw1_q1.html', context={'request': request, 'input_text': input_text, 'qout': "..."})
 
 @app.post('/hw1_q1')
 def hw1_q1_post(request: Request, password: str = Form(""), username: str = Form(""), input_text: str = Form(""), buttonaction: str = Form("")):
-    output = handle_hw_question(timestr  = time.strftime("%Y%m%d_%H%M%S"), 
-                                clientIP = request.client.host, 
+    output = handle_hw_question(timestr  = time.strftime("%Y%m%d_%H%M%S"), clientIP = request.client.host, 
                                 hw_tag   = 'hw1', question_tag = 'q1', testbench_tag = 'hw1_q1_tb.vhdl', # put it under ./testbenches/
                                 username = username, password = password, input_text = input_text, buttonaction = buttonaction);
     return templates.TemplateResponse('hw1_q1.html', context={'request': request, 'input_text': input_text, 'qout': output})
+
+### HW1 - Q2
+@app.get("/hw1_q2")
+def hw1_q2_get(request: Request, password: str = Form(""), username: str = Form(""), input_text: str = Form(""), buttonaction: str = Form("")):
+    return templates.TemplateResponse('hw1_q2.html', context={'request': request, 'input_text': input_text, 'qout': "..."})
+
+@app.post('/hw1_q2')
+def hw1_q2_post(request: Request, password: str = Form(""), username: str = Form(""), input_text: str = Form(""), buttonaction: str = Form("")):
+    output = handle_hw_question(timestr  = time.strftime("%Y%m%d_%H%M%S"), clientIP = request.client.host, 
+                                hw_tag   = 'hw1', question_tag = 'q2', testbench_tag = 'hw1_q2_tb.vhdl', # put it under ./testbenches/
+                                username = username, password = password, input_text = input_text, buttonaction = buttonaction);
+    return templates.TemplateResponse('hw1_q2.html', context={'request': request, 'input_text': input_text, 'qout': output})
+
 
 ###################################################################################################################################################################
 ### Helper Functions
@@ -171,12 +211,34 @@ def handle_hw_question(timestr, clientIP, hw_tag, question_tag, testbench_tag, u
                 with open(hw_q_user_foldername + submissionrecord_filename, "w") as submissionrecord_file:
                     submissionrecord_file.write(submission_state + "\n" + submission_id)
         else:
-            output = "somethings wrong"
-            print("somethings wrong")
+            output = "somethings wrong, handle_hw_question"
+            print("somethings wrong, handle_hw_question")
     else:
         output = msg
 
     return output
+
+def checksubmission_for_hw_q_username(hw_tag, question_tag, username):
+    hw_q_user_foldername             = './submissions/' + hw_tag + '/' + question_tag + '/' + username + '/' 
+    submissionrecord_filename        = 'submission.record'
+    previous_submissionrecord_exists = os.path.isfile(hw_q_user_foldername + submissionrecord_filename);
+    if(previous_submissionrecord_exists):
+        with open(hw_q_user_foldername + submissionrecord_filename, "r") as submissionrecord_file:
+            submission_record = submissionrecord_file.read()
+        
+        if("correct" in submission_record):
+            submission_note = '<font color="#168233"><b>done</b></font>, date_time (UTC):' + "_".join(submission_record.replace("correct\n","").split("_")[0:2])
+        elif("functional error" in submission_record):
+            submission_note = '<font color="#C01C28"><b>functional error</b></font>, date_time (UTC):' + "_".join(submission_record.replace("functional error\n","").split("_")[0:2])
+        elif("syntax error" in submission_record):
+            submission_note = '<font color="#C01C28"><b>syntax error</b></font>, date_time (UTC):' + "_".join(submission_record.replace("syntax error\n","").split("_")[0:2])
+        else:
+            submission_note = "somethings wrong, checksubmission_for_hw_q_username"
+            print("somethings wrong, checksubmission_for_hw_q_username")
+    else:
+        submission_note = '<font color="#2473c7"><b>no submission</b></font>'
+    return submission_note
+
 
 if __name__ == "__main__":
     uvicorn.run("server:app", host="0.0.0.0", port=8080, log_level="info", reload=True, proxy_headers=True, forwarded_allow_ips='*') 
